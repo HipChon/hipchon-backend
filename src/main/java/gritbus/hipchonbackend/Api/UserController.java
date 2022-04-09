540d3e995ccfeb1fb13aed60c735d8a5c4305e07
Mo0nl19ht <mool2480@gmail.com>
2022-04-09 09:57:49 +0900

    fix: ios 이슈 temp

--- src/main/java/gritbus/hipchonbackend/Api/UserController.java
+++ src/main/java/gritbus/hipchonbackend/Api/UserController.java
@@ -39,12 +39,14 @@ public class UserController {
 		@ApiParam(value = "user") @RequestPart(value = "user") UserDto user,
 		@ApiParam(value = "file") @RequestPart(value = "file") MultipartFile multipartFile)
 	{
-		try {
-			return ResponseEntity.ok(userService.save(user,multipartFile));
-		} catch (Exception e){
-			return ResponseEntity.ok(userService.save(user,null));
-		}
+		return ResponseEntity.ok(userService.save(user,multipartFile));
+	}
 
+	@Operation(summary = "회원가입 API temp for ios", description = "")
+	@PostMapping("")
+	public ResponseEntity<String> signUp_temp(
+		@ApiParam(value = "user") @RequestPart(value = "user") UserDto user) {
+		return ResponseEntity.ok(userService.save(user,null));
 	}
 
 	@Operation(summary = "프로필 변경 API", description = "form-data, json형태 key는 user입니다"
